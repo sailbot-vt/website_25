e945cc716575d3066a71157aa2fcaa478c776648
Chris Nassif <chrisjnassif@gmail.com>
2025-06-18 20:50:19 -0400

    fixed some bugs

--- js/all.js
+++ js/all.js
@@ -2,20 +2,6 @@
 const mobileNavbarLinksContainer = document.getElementById("mobile-navbar-links-container")
 const mobileTopNavbar = document.getElementById("mobile-top-navbar")
 let mobileIsDisplayed = false;
-const hamburgerIcon = document.getElementById("hamburger-icon")
-hamburgerIcon.addEventListener('click', function(event){
-   if (mobileIsDisplayed){
-      mobileTopNavbar.classList.add('navhidden')
-      mobileNavbarLinksContainer.classList.remove('mobile-navbar-links-container')
-      mobileIsDisplayed = false;
-   }
-   else{
-       mobileTopNavbar.classList.remove('navhidden')
-       mobileNavbarLinksContainer.classList.add('mobile-navbar-links-container')
-       mobileIsDisplayed = true;
-   }
-})
-
 // Handle navbar scroll effect
 const topNavbar = document.getElementById("top-navbar");
 window.addEventListener('scroll', () => {
