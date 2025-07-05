e5bffa879f98d3c65810afad5f5953cc970496d0
Chris Nassif <chrisjnassif@gmail.com>
2025-07-05 09:47:51 -0400

    refactoring

--- js/fleet.js
+++
@@ -1,7 +0,0 @@
-function changeImage(a) {
-    document.getElementById("boatpicmain").src=a;
-}
-
-function reset() {
-    document.getElementById("boatpicmain").src="../images/fleet/boat_diagram_outline.webp";
-}
